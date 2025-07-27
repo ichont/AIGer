4f818fd19de0af20718c3895b1c73c4ce3da6572
Will Son <143776852+ichont@users.noreply.github.com>
2025-07-27 11:33:12 +0800

    Update AIGermodel.py

--- AIGermodel.py
+++ AIGermodel.py
@@ -8,27 +8,7 @@ from layers import create_spectral_features, MLP
 
 
 class AIGerConv(nn.Module):
-    def __init__(self, in_dim, out_dim, num_relations=2, self_loop=True):
-        super().__init__()
-        self.num_relations = num_relations
-        self.self_loop = self_loop
-
-        self.relation_layers = nn.ModuleList([
-            nn.Linear(in_dim, out_dim, bias=False) for _ in range(num_relations)
-        ])
-
-        if self_loop:
-            self.self_loop_layer = nn.Linear(in_dim, out_dim, bias=False)
-        else:
-            self.self_loop_layer = None
-
-        self.reset_parameters()
-
-    def reset_parameters(self):
-        for layer in self.relation_layers:
-            nn.init.xavier_uniform_(layer.weight)
-        if self.self_loop:
-            nn.init.xavier_uniform_(self.self_loop_layer.weight)
+    # Attention, we will make the source code fully open after the paper is accepted.
 
     def forward(self, x, edge_indices):
         agg = 0
@@ -84,24 +64,6 @@ class AIGer(nn.Module):
         self.readout_prob = MLP(out_dim, out_dim, 1, num_layer=3, p_drop=0.2,
                                 norm_layer='batchnorm', act_layer='relu')
         self.reset_parameters()
-
-    def reset_parameters(self):
-        self.conv1.reset_parameters()
-        for conv in self.convs:
-            conv.reset_parameters()
-        self.weight.reset_parameters()
-
-    def get_x_edge_index(self, init_emb, edge_index_s):
-        self.pos_edge_index = edge_index_s[edge_index_s[:, 2] > 0][:, :2].t()
-        self.neg_edge_index = edge_index_s[edge_index_s[:, 2] < 0][:, :2].t()
-        if init_emb is None:
-            init_emb = create_spectral_features(
-                pos_edge_index=self.pos_edge_index,
-                neg_edge_index=self.neg_edge_index,
-                node_num=self.node_num,
-                dim=self.in_dim
-            ).to(self.device)
-        self.x = init_emb
 
     def forward(self, init_emb, edge_index_s) -> Tuple[Tensor, Tensor]:
         self.get_x_edge_index(init_emb, edge_index_s)
